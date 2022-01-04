96c67f859c1c3dd9776271672de7126fd53f3162
unknown <you@example.com>
2022-01-04 18:38:48 +0700

    favicon

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -49,27 +49,23 @@ export default function Navbar(){
   };
     return (
         <Box sx={{ width: '100%'}}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%'  }}>
-            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-              <Box sx={{marginLeft:60}}><img src="images/Logo_Digit-1.png" width={50}/></Box>
-              
-              <Tab sx={{width:200, marginLeft:20}} label="home" {...a11yProps(1)} />
-              <Tab sx={{width:200}} label="digit" {...a11yProps(2)} />
-              <Tab sx={{width:200}} label="about" {...a11yProps(3)} />
+          <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%', display:'flex'}}>
+          <Box sx={{marginLeft:60}}><img src="images/Logo_Digit-1.png" width={50}/></Box> 
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">  
+              <Tab sx={{width:200, marginLeft:20}} label="home" {...a11yProps(0)} />
+              <Tab sx={{width:200}} label="digit" {...a11yProps(1)} />
+              <Tab sx={{width:200}} label="about" {...a11yProps(2)} />
             </Tabs>
           </Box>
         
-          <TabPanel value={value} index={1}>
+          <TabPanel value={value} index={0}>
             <Home/>
           </TabPanel>
-          <TabPanel value={value} index={2}>
+          <TabPanel value={value} index={1}>
             <Digit/>
           </TabPanel>
-          <TabPanel value={value} index={3}>
+          <TabPanel value={value} index={2}>
             <About/>                                                            
-          </TabPanel>
-          <TabPanel value={value} index={4}>
-            <About/>
           </TabPanel>
         </Box>
       );
